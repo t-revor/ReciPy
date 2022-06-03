9cb953aeba7590c75c6884c4f2b822f17aca4c98
t-revor <48257612+t-revor@users.noreply.github.com>
2022-06-03 19:50:38 +0200

    Update code.py
    Added more comments, everyone love comments.

--- code.py
+++ code.py
@@ -1,10 +1,12 @@
 import pandas as pd
-### The dataset was taken utilizing beautiful soup and parsing web pages from an italian recipe website called "GialloZafferano".
+### The dataset was taken utilizing beautiful soup and parsing web pages from an italian cooking website called "GialloZafferano".
 ### On total there are almost 6000 recipes, ranging from starters to dessert.
 data = pd.read_csv('gz_dataset.csv', sep='|')
 data = data.rename(columns={'link':'preparation'})
 data = data.reset_index()
 
+### This function just makes the user input "readable" by the next lines of code.
+### The idea is to make everything lowercase so it's easier to find all the recipes.
 search = input("""What ingredients do you have?
 
 Separate them with a space [Example: egg sugar]
@@ -15,6 +17,10 @@ def search_split(search):
     return x
 result = search_split(search)
 result
+
+### In the future this loop will be more intuitive.
+### I want to make it so that if >n ingredients are in a recipe they get printed.
+### This way if I have chicken and tuna, for example, the research does not give no output.
 
 for ext in result:
     for i in range(len(data.index)):
